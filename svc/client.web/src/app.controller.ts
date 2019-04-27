af995371677438871c8e1368fbdc5c15d378e459
Gerard Chin <gerardchin91@gmail.com>
2019-04-27 19:19:13 +1200

    x

--- svc/client.web/src/app.controller.ts
+++ svc/client.web/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Req, Render, Inject } from '@nestjs/common';
+import { SrvRequest } from '../../../app/client.web/src/server';
+import { IAppService, APP_SERVICE } from './services/app';
+// import { FastifyRequest, FastifyReply } from "fastify";
+// import { IncomingMessage, ServerResponse } from "http";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject(APP_SERVICE) private appService: IAppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('*')
+  @Render('index.ssr.hbs')
+  public async indexHTML(@Req() req: SrvRequest) {
+    return await this.appService.getHtmlJsonData(<string>req.url, 'en');
   }
+
+  // @Get("*")
+  // public async indexHTML(
+  //   @Request() req: FastifyRequest<IncomingMessage>,
+  //   @Response() res: FastifyReply<ServerResponse>,
+  // ) {
+  //   res.header("Content-Type", "text/html");
+  //   this.appService.streamHtmlPage(res.res, req.req.url || "/");
+  // }
 }
--- svc/client.web/src/app.module.ts
+++ svc/client.web/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_SERVICE } from './services/app';
+import { CONFIG_SERVICE } from './services/config';
+import { AppService } from './services/app/service';
+import { ConfigService } from './services/config/service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    { provide: APP_SERVICE, useClass: AppService },
+    { provide: CONFIG_SERVICE, useClass: ConfigService },
+  ],
 })
 export class ApplicationModule {}
