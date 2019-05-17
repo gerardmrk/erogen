cbaf3f8a371e27bfba09b0411a2a00e29ffaf129
Gerard Chin <gerardchin91@gmail.com>
2019-05-17 22:44:03 +1200

    more snapshots

--- app/client.web/src/client/views/core/AuthRoutesWrapper/component.test.tsx
+++ app/client.web/src/client/views/core/AuthRoutesWrapper/component.test.tsx
@@ -1,3 +1,7 @@
+// Only enable snapshotting when component has stabilised and is not
+// expected to undergo much development changes in the future.
+const SNAPSHOT_ENABLED = true;
+
 import * as React from "react";
 import { ReactTestRenderer, ReactTestInstance } from "react-test-renderer"; // prettier-ignore
 import { createRenderer } from "@client/views/_fixtures_/create-renderer";
@@ -14,6 +18,12 @@ describe("<AuthRoutesWrapper/>", () => {
     renderer = createRenderer(<Component title={"x"} />);
     component = renderer.getInstance() as ReactTestInstance;
   });
+
+  if (SNAPSHOT_ENABLED) {
+    test("snapshot", () => {
+      expect(renderer.toJSON()).toMatchSnapshot();
+    });
+  }
 
   it("renders ok", () => {
     expect(component).toBeDefined();
--- app/client.web/src/client/views/core/GlobalLoader/component.test.tsx
+++ app/client.web/src/client/views/core/GlobalLoader/component.test.tsx
@@ -1,3 +1,7 @@
+// Only enable snapshotting when component has stabilised and is not
+// expected to undergo much development changes in the future.
+const SNAPSHOT_ENABLED = true;
+
 import * as React from "react";
 import { ReactTestRenderer, ReactTestInstance } from "react-test-renderer"; // prettier-ignore
 import { createRenderer } from "@client/views/_fixtures_/create-renderer";
@@ -16,6 +20,12 @@ describe("<GlobalLoader/>", () => {
     );
     component = renderer.getInstance() as ReactTestInstance;
   });
+
+  if (SNAPSHOT_ENABLED) {
+    test("snapshot", () => {
+      expect(renderer.toJSON()).toMatchSnapshot();
+    });
+  }
 
   it("renders ok", () => {
     expect(component).toBeDefined();
